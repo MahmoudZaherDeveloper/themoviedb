a92f01aa49d23c1774b5fdc3a4a1ca5c6144d57e
Mahmoud Zaher <mzaher.c@stcpay.com.sa>
2023-11-23 13:20:22 +0300

    feat(add presentation layer):
    + presentation layer

--- app/src/main/java/com/zaher/cleanwithmvvm/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/zaher/cleanwithmvvm/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.zaher.cleanwithmvvm.ui.theme
+package com.zaher.cleanwithmvvm.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
